0163598b6349ab91e3cde7b9d02e9a9e9463ccd7
Enda Farrell <enda.farrell@gmail.com>
2017-09-16 18:24:34 +0200

    Added GPG Public Key

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,13 +23,13 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
+# Blogroll - Not used
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
          ('You can modify those links in your config file', '#'),)
 
-# Social widget
+# Social widget - Not used
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
